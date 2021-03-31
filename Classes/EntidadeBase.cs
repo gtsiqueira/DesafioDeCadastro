13fc75573e06acc1cbc3082e278b114c60d3b631
gtsiqueira <gildotsiqueira@gmail.com>
2021-03-30 23:56:08 -0300

    Add CRUD de cadastro

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace SeriesCadastro.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+#### *Aplicação de cadastro de série 
+
+###### DESCRIÇÃO
+
+Criar um algoritmo simples de cadastro de séries.
+
+
+
